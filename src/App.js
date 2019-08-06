4881200ca92b1d413854e2c793ff2cf15df9a33b
yuyasaito210 <yuyasaito210@gmail.com>
2019-08-06 04:39:06 -0700

    Initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import OriginalViewChart from './view/originalViewChart';
+
+export class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <OriginalViewChart />
+      </div>
+    )
+  }
+}
+
+export default App;
