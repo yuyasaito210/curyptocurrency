55e59fd0a402ba21ee418bb8c95fb9c30a36aa11
yuyasaito210 <yuyasaito210@gmail.com>
2019-08-07 10:45:44 -0700

    Homepage Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import './App.css';
-import OriginalView from './view/originalViewChart';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Nav, NavDropdown, Navbar, Image } from "react-bootstrap";
 
-function App() {
-  return (
-    <div>
-     <OriginalView/>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand href="#home">BINANACE</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <NavDropdown title="Exchange" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#pricing">Markets</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Register</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+               LogIn
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
-
-export default App;
